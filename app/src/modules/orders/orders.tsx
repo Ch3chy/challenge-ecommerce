b92ba806c4bade3bc7d38d4bd0165f7a47cb8fda
Sergio Luis Alfonso Pinzon <salfonso@fgs.co>
2025-04-08 21:47:47 -0500

    feat(app): protege routes by logged

--- app/src/modules/orders/orders.tsx
+++ app/src/modules/orders/orders.tsx
@@ -1,18 +1,26 @@
 import { Outlet, useNavigate, useParams } from "react-router-dom";
 import Title from "@/base/components/title";
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import styles from "./orders.module.scss";
 import { OrdersList } from "./views/orders-list";
 import { useOrdersStore } from "./stores/useOrdersStore";
+import { useUsersStore } from "../users/stores/useUsersStore";
 
 const Orders: FC = () => {
   const navigate = useNavigate();
   const { orderId } = useParams();
+  const logged = useUsersStore((store) => store.logged);
   const orders = useOrdersStore((store) => store.orders);
 
   const handleGoBack = () => {
     navigate(-1);
   };
+
+  useEffect(() => {
+    if (!logged) {
+      navigate("/");
+    }
+  }, [logged]);
 
   return (
     <section className={styles.orders}>
--- app/src/modules/shopping-cart/shopping-cart.tsx
+++ app/src/modules/shopping-cart/shopping-cart.tsx
@@ -2,14 +2,22 @@ import { FC, useEffect } from "react";
 import styles from "./shopping-cart.module.scss";
 import { Outlet, useNavigate } from "react-router-dom";
 import { useShoppingCartStore } from "./stores/useShoppingCartStore";
+import { useUsersStore } from "../users/stores/useUsersStore";
 
 const ShoppingCart: FC = () => {
   const navigate = useNavigate();
+  const logged = useUsersStore((store) => store.logged);
   const products = useShoppingCartStore((store) => store.products);
 
   useEffect(() => {
     if (products.length <= 0) navigate("/products", { replace: true });
   }, [products]);
+
+  useEffect(() => {
+    if (logged) {
+      navigate("/");
+    }
+  }, [logged]);
 
   return (
     <div className={styles.shoppingCart}>
